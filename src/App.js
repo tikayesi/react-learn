c2c12497d152d8f2120782a88f99219db780a200
tikayesikristiani <yesikristianitika@gmail.com>
2021-12-20 20:38:32 +0700

    hoc understanding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
+import ClickCounter from "./ClickCounter";
+import HoverCounter from "./HoverCounter";
 
 
 class App extends Component{
-
     render(){
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                 </header>
-
+                <ClickCounter/>
+                <HoverCounter/>
             </div>
         );
     }
---
+++ src/ClickCounter.js
@@ -0,0 +1,18 @@
+import {Component} from "react";
+import withCounter from './withCounter'
+
+
+class ClickCounter extends Component{
+
+    render() {
+        const { count, incrementCount } = this.props
+        return(
+            <div>
+                <button onClick={incrementCount}>Click {count} Times</button>
+            </div>
+        )
+    }
+
+}
+
+export default withCounter(ClickCounter);
